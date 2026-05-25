9efbc47870834aa33bf6c055cc0996ace395cd6e
Don Soby <donkiran2255@gmail.com>
2026-05-25 13:11:59 +0530

    Add initial app structure with models, views, and migrations for membership plans

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class MembershipPlan(models.Model):
+
+    PLAN_TYPES = (
+        ("monthly", "Monthly"),
+        ("quarterly", "Quarterly"),
+        ("yearly", "Yearly"),
+    )
+
+    name = models.CharField(max_length=50)
+    duration = models.PositiveIntegerField(help_text="Duration in days")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    plan_type = models.CharField(max_length=20, choices=PLAN_TYPES)
+
+    is_active = models.BooleanField(default=True)
