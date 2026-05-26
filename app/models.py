15e65d1fa9dc4a31d49cd9c6288cf2757ada2af7
Don Soby <donkiran2255@gmail.com>
2026-05-26 17:36:45 +0530

    Refactor MembershipPlan model to update plan types; add Membership and Payment models with relationships

--- app/models.py
+++ app/models.py
@@ -6,13 +6,12 @@ from django.contrib.auth.models import AbstractUser
 class MembershipPlan(models.Model):
 
     PLAN_TYPES = (
-        ('BASIC', 'Basic'),
-        ('PREMIUM', 'Premium'),
-        ('VIP', 'VIP'),
+        ('MONTHLY', 'Monthly'),
+        ('QUATERLY', 'Quaterly'),
+        ('YEARLY', 'Yearly'),
     )
 
     name = models.CharField(max_length=50)
-    duration = models.PositiveIntegerField(help_text="Duration in days")
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     plan_type = models.CharField(max_length=20, choices=PLAN_TYPES)
@@ -33,3 +32,39 @@ class User(AbstractUser):
         choices=ROLE_CHOICES,
         default='USER'
     )
+
+class Membership(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='membership')
+
+    plan = models.OneToOneField(MembershipPlan, on_delete=models.CASCADE, related_name='plan')
+
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.plan.name}"
+    
+
+class Payment(models.Model):
+
+    STATUS_CHOICES = (
+        ("PENDING", "Pending"),
+        ("SUCCESS", "Success"),
+        ("FAILED", "Failed"),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payments")
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+
+    payment_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.status}"
