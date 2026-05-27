b5f90203bed26e739dbad8433bbe56638fc55d3b
Don Soby <donkiran2255@gmail.com>
2026-05-27 10:33:42 +0530

    Refactor membership models and update plan types; add Membership and Payment models with relationships

--- app/models.py
+++ app/models.py
@@ -7,19 +7,25 @@ class MembershipPlan(models.Model):
 
     PLAN_TYPES = (
         ('MONTHLY', 'Monthly'),
-        ('QUATERLY', 'Quaterly'),
+        ('QUARTERLY', 'Quarterly'),
         ('YEARLY', 'Yearly'),
     )
 
     name = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
 
-    plan_type = models.CharField(max_length=20, choices=PLAN_TYPES)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    duration_days = models.PositiveIntegerField()
+
+    plan_type = models.CharField(
+        max_length=20,
+        choices=PLAN_TYPES
+    )
 
     is_active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return f"{self.name}"
 
 
 class User(AbstractUser):
@@ -38,9 +44,9 @@ class User(AbstractUser):
 
 class Membership(models.Model):
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='membership')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='memberships')
 
-    plan = models.ForeignKey(MembershipPlan, on_delete=models.CASCADE, related_name='membership')
+    plan = models.ForeignKey(MembershipPlan, on_delete=models.CASCADE, related_name='memberships')
 
     start_date = models.DateField()
     end_date = models.DateField()
@@ -62,6 +68,8 @@ class Payment(models.Model):
     )
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payments")
+
+    plan = models.ForeignKey(MembershipPlan, on_delete=models.SET_NULL, null=True)
 
     amount = models.DecimalField(max_digits=10, decimal_places=2)
 
