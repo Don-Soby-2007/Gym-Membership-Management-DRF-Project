9475a52d721386ff0740518975e1b50e4276faa0
Don Soby <donkiran2255@gmail.com>
2026-05-27 12:32:44 +0530

    Refactor URLs: reorganize urlpatterns for better readability and maintainability

--- app/urls.py
+++ app/urls.py
@@ -10,6 +10,14 @@ router.register('plans', views.MembershipPlanViewSet, basename='plans')
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('login/refresh', TokenRefreshView.as_view(), name='login_refresh'),
+
     path('register/', views.RegisterAPIView.as_view(), name='register'),
+
+    path('users/', views.UserListView.as_view(), name='users'),
+
+    path('payment/', views.PaymentAPIView.as_view(), name='payment'),
+    path('payment/history', views.PaymentHistoryAPIView.as_view(), name='payment-history'),
+    path('payment/history/<int:user_id>/', views.PaymentHistoryAPIView.as_view(), name='payment-history-user'),
+
     path('', include(router.urls)),
 ]
